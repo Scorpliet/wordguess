fc489370b03a6c79fb0d9e2fde650db60281f08b
Scorpliet <45767308+Scorpliet@users.noreply.github.com>
2021-01-03 17:52:55 +0500

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,12 @@
-alphabet=[['A','B','C','D'], ['E','F','G','H'], ['I','J','K','L'], ['M','N','O','P'], ['Q','R','S','T'], ['U','V','W','X'], ['Y','Z']]
-
+columns=[]
+row1=['A','B','C','D']
+row2=['E','F','G','H']
+row3=['I','J','K','L']
+row4=['M','N','O','P']
+row5=['Q','R','S','T']
+row6=['U','V','W','X']
+row7=['Y','Z']
+alphabet=[row1,row2,row3,row4,row5,row6,row7]
 def tpose(array): #Transpose function i copied from internet
     return [ [row[c] for row in array if c < len(row)] for c in range(0, max([len(row) for row in array])) ]
 
@@ -10,29 +17,56 @@ def printalp():
      print(alphabet[i])
 
 
-printalp()
-length = int(input("Enter the length of your word "))
-maxcolumns=[1,2,3,4,5,6,7]
 
+maxcolumns=[1,2,3,4,5,6,7]
 
 
 def guess():
     columnopt=[]
-    lengthofinput = 1
-    while lengthofinput <=length:
-         guessinp = int(input("Enter the column of the {}".format(lengthofinput) + "th alphabet "))
+    alpt=[]
+    alpt2=[]
+    x = 1
+    while x <=length:
+         guessinp = int(input("Enter the column of the {}".format(x) + "th alphabet "))
          if guessinp >4 or guessinp<1:
              print("Pls enter correct column number")
          else:    
             columnopt.append(guessinp) #adding the input value in a list so it can be fetched later
-            lengthofinput = lengthofinput+1
-            columnopt = list(dict.fromkeys(columnopt)) #a dictionary cannot have duplicate values so we turn it into one and make it back into a list again          
+            x = x+1
+            #columnopt = list(dict.fromkeys(columnopt))  #a dictionary cannot have duplicate values so we turn it into one and make it back into a list
+  
     ranget= len(columnopt)
-    for i in range(ranget):
+    print(columnopt)
+    for i in columnopt:
               alp2=tpose(alphabet) #Transposed the array
-              alpt=[] #Created new array
-              alpt.append(alp2[i]) #added selected columns in new array
-              print(alpt)
+              alpt.append(alp2[i-1]) #added selected columns in new array
+    
+    length_max_value = len(max(alpt, key=len))
+    range_of_array = len(alpt)
+    for i in range(range_of_array):
+        print(alpt[i])
+    columnopt.clear()
+    x=1    
+    
+    while x<=length:
+        guessinp = int(input("Enter the column of the {}".format(x) + "th alphabet "))
+        if guessinp>length_max_value or guessinp<1:
+             print("Pls enter correct column number")
+        else:
+             columnopt.append(guessinp) #adding the input value in a list so it can be fetched later
+             x = x+1
+    range2= len(columnopt)
+    print(columnopt)
+    for i in columnopt:
+            alp3=tpose(alpt)
+            alpt2.append(alp3[i-1])
+    range_of_tpose2 = len(alpt2)
+    columnopt.clear()
+    length_max_value2 = len(max(alpt2, key=len))
+    for i in range(length_max_value2):
+        print(alpt2[i])                
 
-     
-guess()    
+
+printalp()
+length = int(input("Enter the length of your word "))
+guess()
